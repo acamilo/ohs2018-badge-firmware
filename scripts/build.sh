c2d6d1d8ec5fc2c349c4b5a56f21577948ae3188
Alex Camilo <alex.camilo@gmail.com>
2018-09-14 19:03:58 -0400

    debug and travis trigger

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 
 # Install Toolchain
 wget https://dl.espressif.com/dl/xtensa-esp32-elf-linux64-1.22.0-80-g6c4433a-5.2.0.tar.gz
-tar -xzf ~/Downloads/xtensa-esp32-elf-linux64-1.22.0-80-g6c4433a-5.2.0.tar.gz
+tar -xzf xtensa-esp32-elf-linux64-1.22.0-80-g6c4433a-5.2.0.tar.gz
 export PATH="$PATH:`pwd`/xtensa-esp32-elf/bin"
 
 # init submodules
